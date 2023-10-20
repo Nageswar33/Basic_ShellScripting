7623f1777b51ef740312b6104dc42ffc1374f586
Nageswara Rao D <nageswararao.dolla@gmail.com>
2023-10-20 12:09:19 +0530

    Testing Shell

---
+++ test_func.sh
@@ -0,0 +1,6 @@
+a=10
+b=90
+c=$((${a}+${b}))
+echo -e "\e[34m>>>>> Welcome to Function Testing \e[0m"
+echo -e "\e[35m >>>>> Values ${a} , ${b} , ${c} \e[0m"
+echo -e "\e[36m >>>> Emd of the Program \e[0m"
