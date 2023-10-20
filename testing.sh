8cb544ec2fe9f4acee28c4a3175f98421e3d8f15
Nageswara Rao D <nageswararao.dolla@gmail.com>
2023-10-20 11:50:54 +0530

    Testing Shell

--- testing.sh
+++ testing.sh
@@ -4,6 +4,6 @@ b=10
 
 echo -e "\e[32m>>>>>>Welcome to Shell Basic Testing ${name}<<<<<<<<\e[0m"
 echo -e "\e[33m>>>>>>Values of the Variables Now  ${a}  , ${b}  <<<<<<<<\e[0m"
-echo -e "\e[34m>>>>>>Sum of the Variables is  ${a} + {b} <<<<<<<\e[0,"
+echo -e "\e[34m>>>>>>Sum of the Variables is  ${a} + ${b} <<<<<<<\e[0,"
 echo -e "\e[35m>>>>>> Sum has been calculated  <<<<<<<\e[0m"
 echo -e "\e[36m>>>>>>>End of the Program <<<<<<\e[0m"
