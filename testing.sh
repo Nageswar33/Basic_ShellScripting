0cca6604f16179bcd48284d359f548b17f008506
Nageswara Rao D <nageswararao.dolla@gmail.com>
2023-10-20 12:00:08 +0530

    Testing Shell

--- testing.sh
+++ testing.sh
@@ -1,7 +1,7 @@
 a=10
 name=Nageswarao
 b=10
-add= $((${a}+${b}))
+add=$((${a}+${b}))
 
 echo -e "\e[32m>>>>>>Welcome to Shell Basic Testing ${name}<<<<<<<<\e[0m"
 echo -e "\e[33m>>>>>>Values of the Variables Now  ${a}  , ${b}  <<<<<<<<\e[0m"
